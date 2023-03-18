5e0aa3bfdf5e8300d840477978534792e4514cab
sth <sth0067@gmail.com>
2023-03-17 20:25:03 -0500

    updated html, started adding bulmaclasses

---
+++ Assets/script.js
@@ -0,0 +1,42 @@
+
+//bulma example code for the internal storage list -- can we pull the image from ap2 and put in here
+{/* <div class="list">
+  <div class="list-item box">
+    <div class="list-item-image">
+      <figure class="image is-64x64">
+        <img class="is-rounded" src="https://via.placeholder.com/128x128.png?text=Image">
+      </figure>
+    </div>
+
+    <div class="list-item-content">
+      <div class="list-item-title">List item</div>
+      <div class="list-item-description">List item description</div>
+    </div>
+  </div>
+
+  <div class="list-item box">
+    <div class="list-item-image">
+      <figure class="image is-64x64">
+        <img class="is-rounded" src="https://via.placeholder.com/128x128.png?text=Image">
+      </figure>
+    </div>
+
+    <div class="list-item-content">
+      <div class="list-item-title">List item</div>
+      <div class="list-item-description">List item description</div>
+    </div>
+  </div>
+
+  <div class="list-item box">
+    <div class="list-item-image">
+      <figure class="image is-64x64">
+        <img class="is-rounded" src="https://via.placeholder.com/128x128.png?text=Image">
+      </figure>
+    </div>
+
+    <div class="list-item-content">
+      <div class="list-item-title">List item</div>
+      <div class="list-item-description">List item description</div>
+    </div>
+  </div>
+</div> */}
