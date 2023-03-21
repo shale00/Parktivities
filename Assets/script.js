24bfa224b297fc893257eac5ef7796e947e4b6af
Avani <avani.jadeja@gmail.com>
2023-03-21 17:12:01 -0500

    Displaying Park information on page

--- Assets/script.js
+++ Assets/script.js
@@ -31,6 +31,7 @@ $('#stateSearchBtn').click(function(){
       $('#search-results').attr('style', 'display: ;');
       $('#instructions').attr('style', 'display: none;');
       $('#search-results').attr('style', 'display: ;');
+      $('#park-info').attr('style', 'display: none;');
       console.log('display results');
       console.log(selectedState);
 
@@ -76,7 +77,7 @@ $('#actSearchBtn').click(function(){
     } else {return};
   });
 
-
+// display park information
 $("body").on("click", "a.panel-block", function(){
 var selectedParkCode = $(this).data("parkcode");
 console.log("Selected park code : " + selectedParkCode);
@@ -91,18 +92,41 @@ if (selectedParkCode !== '') {
   })
   .then(function(data) {
     
+    
+      $('#instructions').attr('style', 'display: none;');
+      $('#search-results').attr('style', 'display: none;');
+      $('#park-info').attr('style', 'display: block;');
     var parkInfo = data.data[0];
-    var parkAddress = parkInfo.addresses[0];
-    console.log("Park Name:" + parkInfo.fullName);
-    console.log("Park Description: " + parkInfo.description);
-    console.log("Park Activities: ");
-    parkInfo.activities.forEach(function(activity){
-      console.log(activity.name);
-    });
-    console.log("Park addresses: " + parkAddress.line1 + "," + parkAddress.line2 + "," + parkAddress.line3 + "," + parkAddress.city + "," + parkAddress.postalCode + ".");
+    //var parkAddress = parkInfo.addresses[0];
+
+    $("#park-info").html(`<p class="panel-heading is-italic title">${parkInfo.fullName}</p>`);
+
+    $('#park-info').append(`<figure class="image is-128x128">
+    <img src=${parkInfo.images[0].url}></img>
+    </figure>`);
+
+    $('#park-info').append(`<p><b>Park Description:</b>${parkInfo.description}</p>`);
+
+    var parkActivities = "";
+    if(parkInfo.activities.length>0)
+    {
+      parkInfo.activities.forEach(function(activity){
+       parkActivities=parkActivities +activity.name + ",";
+      });
+  
+    }
     
     
-    console.log("Park Image : " + parkInfo.images[0].url);
+    $('#park-info').append(`<p><b>Park Activities:</b>${parkActivities}</p>`);
+
+
+   var parkAddress = parkInfo.addresses[0].line1 + "," + parkInfo.addresses[0].line2 + "," + parkInfo.addresses[0].line3 + "," + parkInfo.addresses[0].city + "," + parkInfo.addresses[0].postalCode + ".";
+
+$('#park-info').append(`<p><b>Park Address:</b>${parkAddress}</p>`);
+
+   
+    
+   
   });
 }  else {return};
 }); 
